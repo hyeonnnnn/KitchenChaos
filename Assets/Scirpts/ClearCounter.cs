628e2a99d558c3cf57f51dfb5643270294db29a4
hyeonnnnn <paige4157@gmail.com>
2024-05-14 19:16:31 +0900

    Kitchen Object Parent

--- Assets/Scirpts/ClearCounter.cs
+++ Assets/Scirpts/ClearCounter.cs
@@ -6,11 +6,49 @@ public class ClearCounter : MonoBehaviour {
 
     [SerializeField] private KitchenObjectSO kitchenObjectSO;
     [SerializeField] private Transform counterTopPoint;
+    [SerializeField] private ClearCounter secondClearCounter;
+    [SerializeField] private bool testing;
+
+    private KitchenObject kitchenObject;
+
+    private void Update() {
+        if (testing && Input.GetKeyDown(KeyCode.T)) {
+            if (kitchenObject != null) {
+                kitchenObject.SetClearCounter(secondClearCounter);
+            }
+        }
+    }
 
     public void Interact() {
-        Transform kitchenObjectTransform = Instantiate(kitchenObjectSO.prefab, counterTopPoint);
-        kitchenObjectTransform.localPosition = Vector3.zero;
+        if (kitchenObject == null) {    // 카운터 위에 물체 X
+            Transform kitchenObjectTransform = Instantiate(kitchenObjectSO.prefab, counterTopPoint);
+            kitchenObjectTransform.GetComponent<KitchenObject>().SetClearCounter(this); // 
+        } else {    // 카운터 위에 물체 O
+            Debug.Log(kitchenObject.GetClearCounter());
+        }
+    }
 
-        Debug.Log(kitchenObjectTransform.GetComponent<KitchenObject>().GetKitchenObjectSO().objectName);
+    public Transform GetKitchenObjectFollowTransform() {
+        return counterTopPoint;
+    }
+
+    // 물체 설정
+    public void SetKitchenObject(KitchenObject kitchenObject) {
+        this.kitchenObject = kitchenObject;
+    }
+
+    // 물체 가져오기
+    public KitchenObject GetKitchenObject() {
+        return kitchenObject;
+    }
+
+    // 물체 없애기
+    public void ClearKitchenObject() {
+        kitchenObject = null;
+    }
+
+    // 물체가 있는지 여부
+    public bool HasKitchenObject() {
+        return kitchenObject != null;
     }
 }
